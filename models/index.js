86c134693c7f1633ee3500324639aa38f585f088
Edita Pesa <editapesa@gmail.com>
2021-05-13 21:42:30 -0400

    comment out relationships in index.js

--- models/index.js
+++ models/index.js
@@ -2,13 +2,13 @@ const User = require('./User');
 const Blog = require('./Blog');
 //add const Comment ?? and relationships below
 
-User.hasMany(Blog, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// User.hasMany(Blog, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-Blog.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Blog.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
 module.exports = { User, Blog };
